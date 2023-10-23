f113c72abb6bbd49097766bd5989b8d230995ba8
Ismar Muslić <ismar.muslic@outlook.com>
2023-10-23 20:57:50 +0200

    update Vector.h

--- NA/Zadace/Zadaca 1/Vector.h
+++ NA/Zadace/Zadaca 1/Vector.h
@@ -19,6 +19,14 @@ public:
     double Norm() const;
     friend double VectorNorm(const Vector &v);
     double GetEpsilon() const;
+    void Print(char separator, double eps) const;
+    friend void PrintVector(const Vector &v, char separator, 
+        double eps);
+    friend Vector operator+(const Vector &v1, const Vector &v2);
+    Vector &operator +=(const Vector &v);
+    friend Vector operator-(const Vector &v1, const Vector &v2);
+    Vector &operator-=(const Vector &v);
+    friend Vector operator*(double s, const Vector &v);
 };
 
 void Vector::Ispisi() {
@@ -65,5 +73,66 @@ double Vector::GetEpsilon() const {
     return 10 * norma * std::numeric_limits<double>::epsilon();
 }
 
+void Vector::Print(char separator = '\n', double eps = -1) const {
+    if(eps<=0) eps = this->GetEpsilon();
+    for(int i=0; i<v.size(); i++) {
+        double trenutni = v.at(i);
+        if(fabs(trenutni)<eps) trenutni = 0;
+        if(i==v.size()-1 && separator != '\n') std::cout<<trenutni;
+        else std::cout<<trenutni<<separator;
+    }
+}
 
+void PrintVector(const Vector &v, char separator = '\n', 
+    double eps = -1) {
+    if(eps<=0) eps = v.GetEpsilon();
+    for(int i=0; i<v.v.size(); i++) {
+        double trenutni = v.v.at(i);
+        if(fabs(trenutni)<eps) trenutni = 0;
+        if(i==v.v.size()-1 && separator != '\n') std::cout<<trenutni;
+        else std::cout<<trenutni<<separator;
+    }
+}
 
+Vector operator+(const Vector &v1, const Vector &v2) {
+    if(v1.v.size()!=v2.v.size())
+        throw std::domain_error("Incompatible formats");
+    Vector rezultat(v1.v.size());
+    for(int i=0; i<v1.v.size(); i++)
+        rezultat.v.push_back(v1.v.at(i)+v2.v.at(i));
+    return rezultat;    
+}
+
+Vector operator-(const Vector &v1, const Vector &v2) {
+    if(v1.v.size()!=v2.v.size())
+        throw std::domain_error("Incompatible formats");
+    Vector rezultat(v1.v.size());
+    for(int i=0; i<v1.v.size(); i++)
+        rezultat.v.push_back(v1.v.at(i)-v2.v.at(i));
+    return rezultat;            
+}
+
+Vector operator*(double s, const Vector &v) {
+    Vector rezultat(v.v.size());
+    for(int i=0; i<v.v.size(); i++) 
+        rezultat.v.at(i)=v.v.at(i)*s;
+    return rezultat;        
+}
+
+Vector &Vector::operator+=(const Vector &v) {
+    if(this->v.size()!=v.v.size())
+        throw std::domain_error("Incompatible formats");
+    for(int i=0; i<this->v.size(); i++) {
+        this->v.at(i)+=v.v.at(i);
+    }
+    return *this;
+}
+
+Vector &Vector::operator-=(const Vector &v) {
+    if(this->v.size()!=v.v.size())
+        throw std::domain_error("Incompatible formats");
+    for(int i=0; i<this->v.size(); i++) {
+        this->v.at(i)-=v.v.at(i);
+    }
+    return *this;
+}
--- NA/Zadace/Zadaca 1/main.cpp
+++ NA/Zadace/Zadaca 1/main.cpp
@@ -1,11 +1,11 @@
 #include <bits/stdc++.h>
 #include "Vector.h"
-
+/*
 double VectorNorm(const Vector &v) {
     long double sumakv = 0;
     for(auto a : v.v) sumakv+=a;
     return sqrt(sumakv);
-}
+}*/
 
 class Matrica {
     std::vector<std::vector<int>> matrica;
@@ -16,13 +16,20 @@ public:
 
 };
 
-
 int main() {
     try {
         //testiranje ovdje:
         Vector a(3);
-        a(0) = 5;
-        std::cout<<a(0)<<"   "<<a(10);
+        Vector b(3);
+        for(int i=0; i<3; i++) {
+            double x; std::cin>>x;
+            a[i]=x;
+            b[i]=x;
+        }
+        std::cout<<"\n"; a+=b;
+        a.Print(); std::cout<<"\n"; b.Print(); std::cout<<"\n";
+        a=operator*(3, a);
+        a.Print();
         //testiranje kraj
     } catch (std::exception &e) {
         std::cout<<e.what();
